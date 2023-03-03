9459140fac67896191c7085abd7420a4e1a57941
erikmint <erik.t3600@hotmail.com>
2023-03-03 19:44:17 -0300

    Add pseudoclass at sass. Rewrite the way task list works.

--- pages/components/ToDoList.tsx
+++ pages/components/ToDoList.tsx
@@ -1,31 +1,33 @@
 export async function getStaticProps() {
     return {
         props: {
-            list: ['']
+            tasks: [{ id: Math.random(), text: '' }],
         }
     }
 }
 
-export default function ToDoList(props: { list: Array<string>; setList: (newArr: Array<string>) => void }) {
+export default function ToDoList(props: {
+    tasks: Array<{ id: number, text: string }>,
+    remove: (id: number) => void,
+    update: (id: number, text: string) => void
+}) {
     return (
         <div className="todo-list">
             <ul>
-                {props.list.map((text: string, indexMap) => {
+                {props.tasks.map((task) => {
                     return (
-                        <li key={Math.random()}>
+                        <li key={task.id}>
                             <div className={"todo-item"}>
                                 <textarea
-                                    defaultValue={text}
-                                    onChange={(value) => props.list[indexMap] = value.target.value}
+                                    defaultValue={task.text}
+                                    onChange={(value) => { props.update(task.id, value.target.value) }}
                                     maxLength={261}
                                     rows={3}>
                                 </textarea>
-                                <input type={'button'}
+                                <input
+                                    type={'button'}
                                     value={'X'}
-                                    onClick={() => {
-                                        const arr = props.list.filter((v, indexFilter) => indexFilter !== indexMap)
-                                        props.setList(arr)
-                                    }}
+                                    onClick={() => { props.remove(task.id) }}
                                 />
                             </div>
                         </li>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,24 +3,44 @@ import ToDoList from "./components/ToDoList"
 
 export default function Main() {
 
-    const [list, setList] = useState([''])
+    const [taskList, setTasklist] = useState([{ id: Math.random(), text: '' }])
+
+    function addTask(taskText: string) {
+        let updated = [...taskList]
+        updated.push({ id: Math.random(), text: taskText })
+        setTasklist(updated)
+    }
+
+    function removeTask(id: number) {
+        const updated = taskList.filter(v => v.id !== id)
+        setTasklist(updated)
+    }
+
+    function updateTask(id: number, text: string) {
+        let task = taskList.find(t => t.id == id)
+        if (!!task) {
+            task.text = text
+            const updatedList = taskList.map(value => {
+                if (value.id == task.id) return task
+                else return value
+            })
+            console.table(updatedList)
+            setTasklist(updatedList)
+        }
+    }
 
     return (
         <>
             <header>
                 <h1>To Do List</h1>
             </header>
-            <ToDoList list={list} setList={setList} />
+            <ToDoList tasks={taskList} update={updateTask} remove={removeTask} />
             <input
                 className="add-item"
                 type={'button'}
                 value={'Add Item'}
-                onClick={() => setList([...list, ''])}
-                onMouseDown={() => {
-                    (document.querySelector('.add-item') as HTMLInputElement).style.backgroundColor = 'blue'
-                }}
-                onMouseUp={() => {
-                    (document.querySelector('.add-item') as HTMLInputElement).style.backgroundColor = '#82954B'
+                onClick={() => {
+                    addTask('')
                 }}
             />
         </>
